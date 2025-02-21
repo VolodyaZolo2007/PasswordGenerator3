11094ef85c31a8acb6e4bc5b9a6cbf8b57d0a90e
Панин.Владимир <v9250578092@gmail.com>
2025-02-21 10:22:43 +0300

    123

---
+++ PasswordGenerator3/AppData/PasswordGeneratorService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PasswordGenerator3.AppData
+{
+    public class PasswordGeneratorService
+    {
+        private readonly Random _random = new Random();
+        private readonly string _number = "1234567890";
+        private readonly string _symbols = "!@#$%^&*()-+_";
+        private readonly string _lowerCharacters = "qwertyuiopasdfghjklzxcvbnm";
+        private readonly string _uppaperCharacters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        private readonly List<string> _patterns;
+
+        public PasswordGeneratorService(bool useNumbers, bool useSymbols, bool useLower, bool useUpper, bool usewords)
+        {
+
+            _patterns = new List<string>();
+
+            if (useNumbers) _patterns.Add(_number);
+            if (useSymbols) _patterns.Add(_symbols);
+            if (useLower) _patterns.Add(_lowerCharacters);
+            if (useUpper) _patterns.Add(_uppaperCharacters);
+
+
+
+        }
+
+        public List<string> Start(int lenght, int passwordsCount)
+        {
+            var passwordSets = new List<string>();
+            for (int i = 0; i < passwordsCount; i++)
+            {
+                string password = string.Empty;
+                while (password.Length < lenght)
+                {
+                    int patternIndex = _random.Next(0, _patterns.Count);
+                    int charIndexFromPattern = _random.Next(0, _patterns[patternIndex].Length);
+
+                    password += _patterns[patternIndex][charIndexFromPattern];
+                }
+                passwordSets.Add(password);
+            }
+
+            return passwordSets;
+
+        }
+
+    }
+}
---
+++ PasswordGenerator3/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using PasswordGenerator3.AppData;
+using System.Windows;
+
+namespace PasswordGenerator3.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PasswordGeneratorService _passwordGeneratorService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void GenerateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _passwordGeneratorService = new PasswordGeneratorService(NumbersCb.IsChecked.Value, SymbolCb.IsChecked.Value, LowerCb.IsChecked.Value, UpperCb.IsChecked.Value, WordsCb.IsChecked.Value);
+            int lenght = int.Parse(PassworLenghtTB.Text);
+            int passwordsCount = int.Parse(passwordsCountTB.Text);
+            PasswordsLb.ItemsSource = _passwordGeneratorService.Start(lenght, passwordsCount);
+
+            if (int.Parse(passwordsCountTB.Text) < 6)
+            {
+                GuardPasswordTB.Text = "Ненадежный";
+
+            }
+            else
+            {
+                bool[] passwordGeneratorService = { NumbersCb.IsChecked.Value, SymbolCb.IsChecked.Value, LowerCb.IsChecked.Value, UpperCb.IsChecked.Value, WordsCb.IsChecked.Value };
+                foreach (bool guard in passwordGeneratorService)
+                {
+                    if (passwordGeneratorService[1])
+                    {
+                        GuardPasswordTB.Text = "Надежный";
+                    }
+                }
+            }
+        }
+    }
+}
